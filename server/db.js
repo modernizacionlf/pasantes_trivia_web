228bf203c77fdf5329c0e2c467c4f3c11b59c783
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-05 13:31:58 +0000

    update database connection variables to use consistent naming

--- server/db.js
+++ server/db.js
@@ -2,17 +2,18 @@ require("dotenv").config()
 
 const { Pool } = require('pg');
 
-const DBUSER = process.env.DBUSER
-const HOST = process.env.HOST
-const DBPASSWORD = process.env.DBPASSWORD
-const DBPORT = process.env.DBPORT
+const POSTGRES_USER = process.env.POSTGRES_USER
+const POSTGRES_HOST = process.env.POSTGRES_HOST
+const POSTGRES_DB = process.env.POSTGRES_DB
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+const POSTGRES_PORT = process.env.POSTGRES_PORT
 
 const pool = new Pool({
-    user: DBUSER,
-    host: HOST,
-    database: 'postgres',
-    password: DBPASSWORD,
-    port: DBPORT
+    user: POSTGRES_USER,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: POSTGRES_PORT
 });
 
 module.exports = pool;
