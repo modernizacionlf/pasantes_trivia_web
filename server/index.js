337aa10931faff3f973c97769c7bade52b348440
7moinfo25 <7moinfo25@gmail.com>
2025-08-05 11:53:17 -0300

    Adición de Imagenes a las Preguntas

--- server/index.js
+++ server/index.js
@@ -16,13 +16,10 @@ app.use(session({
     }
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../uploads/preguntas')));
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
-
 
 const registerRoutes = require('./routes/register');
 const { router: authRoutes } = require('./routes/auth'); 
